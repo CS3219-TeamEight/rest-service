9b6f6ae394e4ee72457d4dec496a5a33ed6d512a
Hooi Tong <hooitongx@gmail.com>
2015-10-09 23:07:51 +0800

    Completed `JobController` to handle job related requests

--- src/main/java/controllers/JobController.java
+++ src/main/java/controllers/JobController.java
@@ -1,14 +1,35 @@
 package controllers;
 
 import entities.Job;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import models.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-public class JobController {
-    @RequestMapping("/job")
-    public Job job(@RequestParam(value="path", defaultValue="EMPTY") String path) {
-        return new Job(path);
+@RestController public class JobController {
+    @Autowired private JobRepository repo;
+
+    @RequestMapping(value = "/job", method = RequestMethod.POST)
+    public Job createJob(@RequestParam(value = "description") String description,
+        @RequestParam(value = "path") String descriptionPath,
+        @RequestParam(value = "title") String title,
+        @RequestParam(value = "password") String password,
+        @RequestParam(value = "visibility") int visibility) {
+        return new Job(description, descriptionPath, title, password, visibility);
+    }
+
+    @RequestMapping(value = "/job/{id}", method = RequestMethod.POST)
+    public Job requestJob(@PathVariable(value = "id") String id,
+        @RequestParam(value = "password") String password) {
+        return repo.findOne(id);
+    }
+
+    @RequestMapping(value = "/job/{id}", method = RequestMethod.PUT)
+    public void updateJob(@PathVariable(value = "id") String id,
+        @RequestParam(value = "password") String password,
+        @RequestParam(value = "visibility") int visibility) {
+        Job foundJob = repo.findOne(id);
+        foundJob.setPassword(password);
+        foundJob.setVisibility(visibility);
+        repo.save(foundJob);
     }
 }
