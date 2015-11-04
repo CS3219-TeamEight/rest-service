35dc542cca8500e33ac40e81ccaf0e903ee02529
Hooi Tong <hooitongx@gmail.com>
2015-11-04 13:21:29 +0800

    Support download of resume as a REST URi

--- src/main/java/com/rankr/controllers/ResumeController.java
+++ src/main/java/com/rankr/controllers/ResumeController.java
@@ -3,11 +3,17 @@ package com.rankr.controllers;
 import com.rankr.entities.Resume;
 import com.rankr.models.ResumeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 
@@ -35,9 +41,30 @@ import java.util.List;
         return null;
     }
 
-    @RequestMapping(value = "/{resumeID}", method = RequestMethod.GET)
-    public Resume getResume(@PathVariable(value = "resumeID") String resumeID) {
-        return repo.findOne(resumeID);
+    @RequestMapping(value = "/{resumeID}", method = RequestMethod.GET, produces = "application/pdf")
+    public ResponseEntity<InputStreamResource> getResume(
+        @PathVariable(value = "resumeID") String resumeID) {
+        Resume retrievedResume = repo.findOne(resumeID);
+        if (retrievedResume == null)
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        else {
+            File resumeFile = new File(retrievedResume.getResumePath());
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+            headers.add("Pragma", "no-cache");
+            headers.add("Expires", "0");
+            headers.add("Content-Disposition",
+                "attachment; filename=\"" + resumeFile.getName() + "\"");
+
+            try {
+                return ResponseEntity.ok().headers(headers).contentLength(resumeFile.length())
+                    .contentType(MediaType.parseMediaType("application/octet-stream"))
+                    .body(new InputStreamResource(new FileInputStream(resumeFile)));
+            } catch (IOException e) {
+                System.err.println("Failed to retrieve resume: " + e.getMessage());
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
+        }
     }
 
     /* TODO: Add authentication */
